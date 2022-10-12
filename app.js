36a389b96ba57560120ddf904ba6c7aae180f90a
gregfenton <greg.fenton@gmail.com>
2022-10-12 03:43:47 -0400

    Update app.js
    fix syntax error

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(sayHello));
 
-app.post("/handle_form", (req, res) => res.type('html').send(handleForm(req));
+app.post("/handle_form", (req, res) => res.type('html').send(handleForm(req)));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
